0de9a77f926fccab54034ff035242666d92c9661
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-08-04 19:14:37 +0200

    added initial api

---
+++ G2/Class 1/ASPNETAPI-G2-L1/ASPNETAPI-G2-L1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETAPI_G2_L1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray());
+        }
+
+        [HttpGet]
+        [Route("GetFreezingTemperature")]
+        public IActionResult GetFreezingTemperature()
+        {
+            return Ok(-2f);
+        }
+    }
+}
