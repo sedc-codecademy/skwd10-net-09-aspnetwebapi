14ce57967ca0ffe40024f8928e91e0b3b89538f4
tstojanovska <stojanovskatanja03@gmail.com>
2022-08-06 13:22:41 +0200

    Class01

---
+++ G4/Class01/Code/OurFirstApi/OurFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("strings")]
+        public IEnumerable<string> GetStrings()
+        {
+            return new List<string> { "value1", "value2" };
+        }
+    }
+}
