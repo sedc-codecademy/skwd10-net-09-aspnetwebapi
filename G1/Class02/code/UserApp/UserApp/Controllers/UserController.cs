fc0f62d0b093347b11689f5c8e304fe11532a413
vjakovlev <viktor_jakovlev@yahoo.com>
2022-08-10 19:53:58 +0200

    code uploaded

---
+++ G1/Class02/code/UserApp/UserApp/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //https://localhost:7023/api/user/getall
+        [HttpGet("GetAll")]
+        public IActionResult GetAllUsers() 
+        {
+            var response = StaticDB.Users;
+
+            if (response.Count < 1) 
+            {
+                return BadRequest("There are 0 users in the databse!");
+            }
+
+            return Ok(response);
+        }
+        //from route
+        //https://localhost:7023/api/user/getuser/1
+        [HttpGet("GetUser/{id}")]
+        public IActionResult GetUserByIdRoute([FromRoute] int id) 
+        {
+            var user = StaticDB.Users.FirstOrDefault(user => user.Id == id);
+
+            if (user == null) 
+            {
+                return NotFound($"User with Id {id} was not found in the database!");
+            }
+
+            return Ok(user);
+        }
+
+        //from query
+        //https://localhost:7023/api/user/getuser?id=2
+        [HttpGet("GetUser")]
+        public IActionResult GetUserByIdQuery([FromQuery] int id) 
+        {
+            var user = StaticDB.Users.FirstOrDefault(user => user.Id == id);
+
+            if (user == null)
+            {
+                return NotFound($"User with Id {id} was not found in the database!");
+            }
+
+            return Ok(user);
+        }
+
+
+        //https://localhost:7023/api/user/test/route1/viktor/route2/radmila?query1=igor&query2=123
+        [HttpGet("test/route1/{route1}/route2/{route2}")]
+        public IActionResult TestRoutAndQueryParameters([FromRoute] string route1,
+                                                        [FromRoute] string route2,
+                                                        [FromQuery] string query1,
+                                                        [FromQuery] int query2) 
+        {
+            var response = $"{route1} - {route2} - {query1} - {query2}";
+            return Ok(response);
+        }
+    }
+}
---
+++ G1/Class02/code/UserApp/UserApp/StaticDB.cs
@@ -0,0 +1,31 @@
+﻿using UserApp.Models;
+
+namespace UserApp
+{
+    public class StaticDB
+    {
+        public static List<User> Users = new List<User>
+        {
+            new User 
+            {
+                Id = 1,
+                Name = "Viktor",
+                Password = "123456"
+            },
+            new User
+            {
+                Id = 2,
+                Name = "Igor",
+                Password = "igor123"
+            },
+            new User
+            {
+                Id = 3,
+                Name = "Petar",
+                Password = "444444"
+            }
+        };
+
+
+    }
+}
