c1cba0f8178686b2d622feaaffe7bed6d73864f5
Trajan Stevkovski <Trajan.Stevkovski@endava.com>
2022-08-15 21:07:32 +0200

    Class03

---
+++ G3/Class03/SEDC.WebApi.Class03.1/SEDC.WebApi.Class03.1.App/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SEDC.WebApi.Class03._1.App.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ G3/Class03/SEDC.WebApi.Class03.1/SEDC.WebApi.Class03.1.App/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SEDC.WebApi.Class03._1.App.Models
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+    }
+}
