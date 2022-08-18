c9c88ce87a7309ce26ce7892909da4fee8bfb502
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-08-18 20:13:53 +0200

    refactor and sql like linq

---
+++ G2/Class 6/ASPNETAPI-G2-L6/ASPNETAPI-G2-L6/Contracts/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPNETAPI_G2_L6.Contracts
+{
+    public class UserDto
+    {
+        public int Id { get; set; } 
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
--- G2/Class 6/ASPNETAPI-G2-L6/ASPNETAPI-G2-L6/Controllers/UsersController.cs
+++ G2/Class 6/ASPNETAPI-G2-L6/ASPNETAPI-G2-L6/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
-﻿using ASPNETAPI_G2_L6.Database;
-using ASPNETAPI_G2_L6.Models;
-using Microsoft.AspNetCore.Http;
+﻿using ASPNETAPI_G2_L6.Contracts;
+using ASPNETAPI_G2_L6.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,19 +17,48 @@ namespace ASPNETAPI_G2_L6.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public async Task<IActionResult> GetUserInfo([FromRoute]int id)
+        public async Task<IActionResult> GetUserBaseInfo([FromRoute] int id)
         {
-            User user = await _notesDbContext.Users
-                                             .Include(x => x.Notes)
-                                             .SingleOrDefaultAsync(x => x.Id == id);
-            
+            UserDto? userDto = await _notesDbContext.Users.Select(x => new UserDto()
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName
+            }).SingleOrDefaultAsync(x => x.Id == id);
+
+            if (userDto is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userDto);
+        }
+
+        [HttpGet]
+        [Route("{id}/details")]
+        public async Task<IActionResult> GetUserDetails([FromRoute] int id)
+        {
+            //User? user = await _notesDbContext.Users
+            //                                 .Include(x => x.Notes)
+            //                                 .SingleOrDefaultAsync(x => x.Id == id);
+
+            var user = await (from dbUser in _notesDbContext.Users
+                              join dbNote in _notesDbContext.Notes
+                              on dbUser.Id equals dbNote.UserId
+                              where dbUser.Id == id
+                              select new
+                              {
+                                  dbNote,
+                                  dbUser
+                              }).SingleOrDefaultAsync();
+
             if (user is null)
             {
                 return NotFound();
             }
 
             return Ok(user);
-        
+
         }
     }
 }
