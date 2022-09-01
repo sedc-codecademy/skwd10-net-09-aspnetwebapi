cd560158f5cf84e3852d063d50bd5fef1b4cebae
Bojan Damcevski <bojandamcevski@gmail.com>
2022-09-01 19:04:03 +0200

    Class 11

---
+++ G2/Class 11/NotesProject/Notes.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Notes.Domain.Entities;
+
+namespace Notes.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IReadOnlyList<User>> GetAllUsersAsync();
+
+        Task<User> GetUserAsync(int id);
+
+        void InsertUser(User newUser);
+
+        Task UpdateUserAsync(User existingUser);
+
+        Task DeleteUserAsync(int id);
+
+        Task<User> GetUserByUsernameAsync(string username);
+    }
+}
---
+++ G2/Class 11/NotesProject/Notes.Storage/Repositories/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Notes.Domain.Entities;
+using Notes.Storage.Database;
+
+namespace Notes.Storage.Repositories
+{
+    public abstract class RepositoryBase<T> where T : BaseEntity
+    {
+        protected readonly INotesDbContext _notesDbContext;
+
+        public RepositoryBase(INotesDbContext notesDbContext)
+        {
+            _notesDbContext = notesDbContext;
+        }
+
+        protected IQueryable<T> GetById(int id)
+        {
+            return GetAll().Where(x => x.Id == id);
+        }
+
+        protected IQueryable<T> GetAll()
+        {
+            return _notesDbContext.Set<T>();
+        }
+
+        protected void InsertEntity(T item)
+        {
+            _notesDbContext.Set<T>().Add(item);
+        }
+
+        protected void UpdateEntity(T item)
+        {
+            _notesDbContext.Set<T>().Update(item);
+        }
+
+        protected void DeleteEntity(int id)
+        {
+            _notesDbContext.Set<T>().Remove(GetById(id).SingleOrDefault());
+        }
+    }
+}
