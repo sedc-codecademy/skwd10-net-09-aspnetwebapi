cde7a2c4f7bb446a449890b3c9ad3f2e3fd9c879
Bojan Damcevski <bojandamcevski@gmail.com>
2022-09-03 12:09:10 +0200

    Class 12

---
+++ G2/Class 12/NotesProject/Notes.Storage/Repositories/TagRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain.Entities;
+using Notes.Domain.Repositories;
+using Notes.Storage.Database;
+
+namespace Notes.Storage.Repositories
+{
+    public class TagRepository : RepositoryBase<Tag>, ITagRepository
+    {
+        public TagRepository(INotesDbContext notesDbContext) : base(notesDbContext)
+        {
+
+        }
+
+        public async Task DeleteTagAsync(int id)
+        {
+            DeleteEntity(id);
+        }
+
+        public async Task<IReadOnlyList<Tag>> GetAllTagsAsync()
+        {
+            IQueryable<Tag> tagQuery = GetAll();
+
+            return await tagQuery.ToArrayAsync();
+        }
+
+        public async Task<Tag> GetTagAsync(int id)
+        {
+            return await GetById(id).SingleOrDefaultAsync();
+        }
+
+        public async Task InsertTagAsync(Tag newTag)
+        {
+            InsertEntity(newTag);
+        }
+
+        public async Task UpdateTagAsync(Tag existingTag)
+        {
+            UpdateEntity(existingTag);
+        }
+    }
+}
---
+++ G2/Class 12/NotesProject/Notes.Tests/UserControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Notes.Contracts.DTOs;
+using Notes.Contracts.DTOs.UserDtos;
+using Notes.Contracts.Services;
+using NotesApi.Controllers;
+using System.Threading.Tasks;
+
+namespace Notes.Tests
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        [TestMethod]
+        public void RegisterUserWithEmptyRegisterDtoShouldNotCreateUser()
+        {
+            //Arrange
+            INoteService noteService = Mock.Of<INoteService>();
+            IUserService userService = Mock.Of<IUserService>();
+
+            UsersController usersController = new UsersController(userService, noteService);
+            RegisterUserDto registerUserDto = null;
+
+            //Act
+            IActionResult result = usersController.RegisterUser(registerUserDto);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public async Task GetNoteShouldReturnNoteWhenIdIsPassed()
+        {
+            //Arrange
+            NoteDto noteDto = new NoteDto()
+            {
+                Id = 1,
+                Description = "Description1",
+                Name = "Note1"
+            };
+
+            Mock<INoteService> noteService = new Mock<INoteService>();
+            noteService.Setup(note => note.GetNoteAsync(1)).Returns(Task.FromResult(noteDto));
+            IUserService userService = Mock.Of<IUserService>();
+
+            UsersController usersController = new UsersController(userService, noteService.Object);
+
+            //Act
+            ActionResult<NoteDto> result = await usersController.GetNote(noteDto.Id);
+
+            //Assert
+            Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+            Assert.AreEqual(result.Value.Id, noteDto.Id);
+            Assert.AreEqual(result.Value.Name, noteDto.Name);
+            Assert.AreEqual(result.Value.Description, noteDto.Description);
+        }
+    }
+}
