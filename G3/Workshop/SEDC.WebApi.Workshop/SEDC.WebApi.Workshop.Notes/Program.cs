2424ce22d5986ea35bcc7f3803b045d22009cb32
Trajan Stevkovski <Trajan.Stevkovski@endava.com>
2022-09-15 18:15:02 +0200

    class15

--- G3/Workshop/SEDC.WebApi.Workshop/SEDC.WebApi.Workshop.Notes/Program.cs
+++ G3/Workshop/SEDC.WebApi.Workshop/SEDC.WebApi.Workshop.Notes/Program.cs
@@ -93,11 +93,14 @@ namespace SEDC.WebApi.Workshop.Notes
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseAuthentication();
             app.UseAuthorization();
